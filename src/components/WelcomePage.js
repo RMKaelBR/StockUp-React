c01c77c6169af95121bc2f2d73a118bdaa0a9191
RMKaelBR <rmbresurreccion@gmail.com>
2023-08-31 21:12:57 +0800

    adjust spacing

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -24,13 +24,13 @@ function Navigation() {
           className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
           onClick={handleLoginClick}
         >
-              Log-in
+          Log-in
         </button>
         <button 
           className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
           onClick={handleSignupClick}
         >
-              Sign Up
+          Sign Up
         </button>
       </div>
       
