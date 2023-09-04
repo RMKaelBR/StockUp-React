486eac7c48d99d234a604e761657a3bc3e4512ed
RMKaelBR <rmbresurreccion@gmail.com>
2023-09-04 21:44:40 +0800

    Updates

---
+++ precache-manifest.bcf43ec31371a45a4dacf1b34318f50e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9829f0785058f5d00c5",
+    "url": "/StockUp-React/static/css/main.80937121.chunk.css"
+  },
+  {
+    "revision": "c9829f0785058f5d00c5",
+    "url": "/StockUp-React/static/js/main.48a752d4.chunk.js"
+  },
+  {
+    "revision": "c13d91b61935ee030f0c",
+    "url": "/StockUp-React/static/js/runtime~main.ec02030f.js"
+  },
+  {
+    "revision": "40bf78962117d5c58122",
+    "url": "/StockUp-React/static/js/2.daa19376.chunk.js"
+  },
+  {
+    "revision": "f4866491ae15624a43d3",
+    "url": "/StockUp-React/static/js/3.b700955a.chunk.js"
+  },
+  {
+    "revision": "7ed6b86a4ae93b7c874954300f8e75a9",
+    "url": "/StockUp-React/static/media/stockup.7ed6b86a.png"
+  },
+  {
+    "revision": "8aba59314170464248dd98cdae4d8757",
+    "url": "/StockUp-React/index.html"
+  }
+];
